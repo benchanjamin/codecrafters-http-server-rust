f9d69be01ae9af5d05c8463738742abcbb6c54ab
Ben Chan <47726262+benchanjamin@users.noreply.github.com>
2024-05-24 18:08:09 -0500

    3nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{prelude::*, BufReader};
+use std::net::TcpStream;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,13 +13,36 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(_stream) => {
                 println!("accepted new connection");
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+                handle_connection(_stream);
+                // _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("{:#?}", http_request);
+
+    let start_line = http_request.first().unwrap();
+    let path = start_line.split_whitespace().nth(1).unwrap();
+    let response: &str;
+    if path == "/" {
+        response = "HTTP/1.1 200 OK\r\n\r\n";
+    } else {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
