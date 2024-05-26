43cf5052592bec7b683ec6c0dd657101f82859a3
Ben Chan <47726262+benchanjamin@users.noreply.github.com>
2024-05-25 23:26:12 -0500

    10th stage

--- src/main.rs
+++ src/main.rs
@@ -1,170 +1,157 @@
-use anyhow::Error;
-use std::{
-    env, fs,
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-    str, thread,
-};
-use std::fs::{File};
+use anyhow::Result;
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::str;
 
-#[derive(Debug)]
-struct HttpRequest {
-    method: String,
-    path: String,
-    version: String,
-    host: Option<String>,
-    user_agent: Option<String>,
-    other_headers: Vec<(String, String)>,
-    contents: String,
+use itertools::Itertools;
+
+const RESPONSE_404: &str = "HTTP/1.1 404 Not Found\r\n\r\n";
+const RESPONSE_200: &str = "HTTP/1.1 200 OK";
+const RESPONSE_201: &str = "HTTP/1.1 201 Created";
+
+fn handle_request(mut stream: TcpStream, request: String, dir: String) {
+    let (first_line, rest_lines) = request.split_once("\r\n").unwrap();
+    let (method, rest) = first_line.split_once(" ").unwrap();
+
+    let response = match method {
+        "GET" => match rest.split_once(" ") {
+            Some((path, _)) => {
+                if path == "/" {
+                    format!("{}\r\n\r\n", RESPONSE_200).into_bytes()
+                } else if path.starts_with("/echo") {
+                    let word = path.strip_prefix("/echo/").unwrap();
+
+                    let encoding = rest_lines
+                        .split("\r\n")
+                        .find(|line| line.starts_with("Accept-Encoding"));
+
+                    match encoding {
+                        Some(encoding) => {
+                            if encoding.contains("gzip") {
+                                let mut encoder = GzEncoder::new(vec![], Compression::default());
+                                encoder.write_all(word.as_bytes()).unwrap();
+
+                                let encoded_content = encoder.finish().unwrap();
+
+                                let headers = format!(
+                                    "{}\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: {}\r\n\r\n",
+                                    RESPONSE_200,
+                                    encoded_content.len(),
+                                );
+
+                                let mut response = headers.into_bytes();
+                                response.extend_from_slice(&encoded_content);
+                                response
+                            } else {
+                                format!(
+                                    "{}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                                    RESPONSE_200,
+                                    word.len(),
+                                    word
+                                ).into_bytes()
+                            }
+                        }
+                        None => format!(
+                            "{}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                            RESPONSE_200,
+                            word.len(),
+                            word
+                        )
+                            .into_bytes(),
+                    }
+                } else if path.starts_with("/user-agent") {
+                    let user_agent = rest_lines
+                        .split("\r\n")
+                        .find(|line| line.starts_with("User-Agent"))
+                        .unwrap()
+                        .strip_prefix("User-Agent: ")
+                        .unwrap();
+
+                    format!(
+                        "{}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        RESPONSE_200,
+                        user_agent.len(),
+                        user_agent
+                    )
+                        .into_bytes()
+                } else if path.starts_with("/files") {
+                    let file = path.strip_prefix("/files").unwrap();
+
+                    match std::fs::read_to_string(format!("{}/{}", dir, file)) {
+                        Ok(content) => {
+                            format!(
+                                "{}\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                                RESPONSE_200,
+                                content.len(),
+                                content
+                            ).into_bytes()
+                        }
+                        Err(_) => RESPONSE_404.into(),
+                    }
+                } else {
+                    RESPONSE_404.into()
+                }
+            }
+            None => "HTTP/1.1 400 Bad Request\r\n\r\n".into(),
+        },
+        "POST" => match rest.split_once(" ") {
+            Some((path, _)) => {
+                if path.starts_with("/files") {
+                    let fname = path.strip_prefix("/files").unwrap();
+                    let mut file = File::create(format!("{}/{}", dir.to_owned(), fname)).unwrap();
+
+                    let content = rest_lines.split_once("\r\n\r\n").unwrap().1;
+
+                    dbg!(content);
+
+                    file.write_all(content.as_bytes()).unwrap();
+
+                    format!("{}\r\n\r\n", RESPONSE_201).into_bytes()
+                } else {
+                    RESPONSE_404.into()
+                }
+            }
+            None => "HTTP/1.1 400 Bad Request\r\n\r\n".into(),
+        },
+        _ => "HTTP/1.1 405 Method Not Allowed\r\n\r\n".into(),
+    };
+
+    stream.write(&response).unwrap();
 }
-impl Default for HttpRequest {
-    fn default() -> Self {
-        HttpRequest {
-            user_agent: Default::default(),
-            host: Default::default(),
-            method: Default::default(),
-            version: Default::default(),
-            path: Default::default(),
-            other_headers: Default::default(),
-            contents: Default::default(),
-        }
-    }
-}
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+
+fn main() -> Result<()> {
     println!("Logs from your program will appear here!");
+
+    let args = env::args().collect_vec();
+
+    let dir = if args.len() > 1 && args[1] == "--directory" {
+        args[2].clone()
+    } else {
+        String::from("")
+    };
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                thread::spawn(|| handle_connect(_stream));
+            Ok(mut s) => {
+                println!("new client!");
+                let mut request = [0_u8; 1024];
+                let bytes = s.read(&mut request).unwrap();
+                let request = String::from_utf8_lossy(&request[..bytes]).into_owned();
+                dbg!(&request);
+
+                handle_request(s, request, dir.clone());
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
-}
-fn handle_connect(mut stream: TcpStream) {
-    println!("accepted new connection");
 
-    let mut buffer = [0; 1024];
-    let ok_resp = "HTTP/1.1 200 OK\r\n\r\n".to_string();
-    let not_found_resp = "HTTP/1.1 404 Not Found\r\n\r\n".to_string();
-
-    match stream.read(&mut buffer) {
-        Ok(_n) => {
-            println!("Req received");
-            let req = str::from_utf8(&buffer).unwrap();
-            let http_request = pars_req(&req).unwrap();
-            let mut resp = not_found_resp.clone();
-            // println!("path: {:#?}", http_request.user_agent);
-
-            if http_request.path == "/" {
-                resp = ok_resp.clone();
-            } else if http_request.path.starts_with("/echo") {
-                let body = http_request.path.replace("/echo/", "");
-                let mut headers = "".to_string();
-
-                for (key, value) in http_request.other_headers.iter() {
-                    dbg!(key, value);
-                    if key == "Accept-Encoding" && value == "gzip" {
-                        headers += format!("Content-Encoding: {value}\r\n").as_str();
-                    }
-                }
-
-                headers += format!("Content-Type: text/plain\r\nContent-Length: {}\r\n", body.len()).as_str();
-
-
-                // dbg!(body.clone());
-
-                resp = format!(
-                    "HTTP/1.1 200 OK\r\n{}\r\n{}",
-                    headers,
-                    body
-                );
-                // dbg!(resp.clone());
-            } else if http_request.path.starts_with("/user-agent") {
-                let body = http_request.user_agent.unwrap();
-                if http_request.method == "GET" {
-                    resp = format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
-                        body.len(),
-                        body
-                    )
-                }
-            } else if http_request.method == "GET" && http_request.path.starts_with("/files") {
-                let file_name = http_request.path.replace("/files/", "");
-                let env_args: Vec<String> = env::args().collect();
-                let mut dir = env_args[2].clone();
-                dir.push_str(&file_name);
-                let file = fs::read(dir);
-                match file {
-                    Ok(fc) => {
-                        resp = format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}\r\n", fc.len(), String::from_utf8(fc).expect("file content"));
-                    }
-                    Err(..) => resp = not_found_resp.clone(),
-                }
-            } else if http_request.method == "POST" && http_request.path.starts_with("/files"){
-                let file_name = http_request.path.replace("/files/", "");
-                let body = http_request.contents.split_once("\r\n\r\n").unwrap().1;
-                let body = body.split_once("\0").unwrap().0;
-                // dbg!(body);
-                let env_args: Vec<String> = env::args().collect();
-                let mut dir = env_args[2].clone();
-                dir.push_str(&file_name);
-                let mut file = File::create(dir).unwrap();
-                file.write_all(body.as_bytes()).unwrap();
-                resp = "HTTP/1.1 201 Created\r\n\r\n".to_string();
-            }
-
-            match stream.write(resp.as_bytes()) {
-                Ok(_) => println!("Ok"),
-                Err(e) => println!("err: {}", e),
-            }
-
-        },
-        Err(e) => println!("Fail connect: {}", e),
-    }
-}
-fn pars_req(req: &str) -> Result<HttpRequest, Error> {
-    let content: Vec<&str> = req.lines().collect();
-    let mut method_header = content[0].split_whitespace();
-    let host = content[1].replace("Host: ", "");
-    let user_agent = content[2].replace("User-Agent: ", "");
-
-    // println!("content: {:?}", content);
-
-    let mut other_headers: Vec<_> = vec![];
-    // for each in content, split by ": " and put values in a tuple in a vector
-    for i in 0..content.len() {
-        if content[i].is_empty() {
-            break;
-        }
-        // println!("content[i]: {:?}", content[i]);
-        let header = content[i].split_once(": ");
-        match header {
-            Some(h) => {
-                // println!("header: {:?}", h);
-                let h_1_expanded = h.1.split(", ").collect::<Vec<&str>>();
-                for h_1 in h_1_expanded {
-                    other_headers.push((h.0.to_string(), h_1.to_string()));
-                }
-                // other_headers.push((h.0.to_string(), h.1.to_string()));
-            }
-            None => {}
-        }
-    }
-
-    let http_request = HttpRequest {
-        method: String::from(method_header.next().unwrap()),
-        path: String::from(method_header.next().unwrap()),
-        version: String::from(method_header.next().unwrap()),
-        host: Some(host),
-        user_agent: Some(user_agent),
-        other_headers,
-        contents: req.to_string(),
-        ..Default::default()
-    };
-    Ok(http_request)
+    Ok(())
 }
